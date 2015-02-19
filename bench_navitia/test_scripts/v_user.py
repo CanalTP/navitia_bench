170161074a827c1452faf6e864fda6654157dca8
antoine-de <antoine.desbordes@gmail.com>
2015-02-19 17:32:25 +0100

    Bench working with multimech
    to run:
    PYTHONPATH=. CONFIG_FILE=dev_settings multimech-run bench_navitia

---
+++ bench_navitia/test_scripts/v_user.py
@@ -0,0 +1,39 @@
+from contextlib import contextmanager
+import os
+import random
+import time
+from server import Server
+
+
+if os.environ.get('CONFIG_FILE'):
+    conf = __import__(os.environ.get('CONFIG_FILE'))
+else:
+    import settings as conf
+
+
+@contextmanager
+def time_that(trans, timer_name, instance=None):
+    """
+    measure time of all work done under the 'with'
+    """
+    start_time = time.time()
+    yield
+    elapsed_time = time.time() - start_time
+    if instance:
+        trans.custom_timers[timer_name][instance] = elapsed_time
+    else:
+        trans.custom_timers[timer_name] = elapsed_time
+
+
+class Transaction(object):
+    def __init__(self):
+        self.api = Server(conf.URL, conf.TOKEN)
+
+    def run(self):
+
+        for r in self.api.regions.itervalues():
+            with time_that(self, 'journeys_' + r.id_):
+                res = r.journeys.get_random()
+
+                assert res.status_code == 200
+
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import requests
 from region import Region
 
+
 class Server:
     def __init__(self, host, token):
         self.host = host
@@ -10,13 +11,18 @@ class Server:
 
     def query(self, url):
         full_url = "{}/{}".format(self.host, url)
-        print full_url
         return requests.get(full_url, auth=(self.token, ''))
 
     def set_regions(self):
         if not self.regions:
             r = self.query('v1/coverage')
             for region in r.json()['regions']:
-                self.regions[region['id']] = Region(self, region['id'],
-                region['start_production_date'],
-                region['end_production_date'])
+                r_id = region['id']
+
+                if region['status'] != 'running':
+                    print "region {} is not running, skipping it".format(r_id)
+                    continue
+
+                self.regions[r_id] = Region(self, r_id,
+                                            region['start_production_date'],
+                                            region['end_production_date'])
