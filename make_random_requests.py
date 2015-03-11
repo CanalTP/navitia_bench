e81635fa8e71b7fb9b815a7516481aae8a2131c1
Vincent Lara <lara.vincent@gmail.com>
2015-03-11 17:43:25 +0100

    Add make_random_requests

---
+++ make_random_requests.py
@@ -0,0 +1,16 @@
+from server import Server
+from stop_areas import StopAreas
+import os, csv
+
+if os.environ.get('CONFIG_FILE'):
+    conf = __import__(os.environ.get('CONFIG_FILE'))
+else:
+    import settings as conf
+
+s = Server(conf.URL, conf.TOKEN)
+
+with open('source.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for region in s.regions.itervalues():
+        for i in xrange(1000):
+            writer.writerow(region.journeys.make_random_request())
