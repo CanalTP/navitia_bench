d9614a5166bcc5b3875051d05c7e3049f473d62c
Vincent Lara <lara.vincent@gmail.com>
2015-03-11 18:33:58 +0100

    Journeys: add make_random_requests

--- journeys.py
+++ journeys.py
@@ -5,7 +5,7 @@ class Journeys:
     def __init__(self, region):
         self.region = region
 
-    def get_random(self):
+    def make_random_request(self):
         from_ = self.region.places.get_random_id()
         to_ = self.region.places.get_random_id()
         while to_ == from_:
@@ -15,8 +15,15 @@ class Journeys:
         minutes = random.randrange(60)
         delta = timedelta(days=p, hours=hours, minutes=minutes)
         date = (self.region.start_production + delta).strftime('%Y%m%dT%H%M00')
+        return (from_, to_, date)
+
+    def get_random(self):
+        from_, to_, date = self.make_random_request()
         return self.get(from_, to_, date)
 
     def get(self, from_, to_, date):
-        return self.region.query('journeys?from={}&to={}&datetime={}'.format(from_, to_, date))
+        return self.region.query('journeys?from={}&to={}&datetime={}&min_nb_journeys=3'.format(from_, to_, date))
+
+    def simple(self, from_, to_, date):
+        return self.region.query('journeys?from={}&to={}&datetime={}&min_nb_journeys=3&simple=true'.format(from_, to_, date))
 
