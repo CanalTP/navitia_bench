6e1e57888d525ba4dd8a2213e6edb1aaf4fad8c0
Vincent Lara <lara.vincent@gmail.com>
2015-03-12 12:40:47 +0100

    Update config

--- dev_settings.py
+++ dev_settings.py
@@ -1,6 +1,7 @@
+import os
 TOKEN='da821360-5fdc-47cb-acc3-9025e56fd711'
 URL='navitia2-ws.ctp.dev.canaltp.fr'
 PORT='80'
-JMX_SCRIPT='/home/jenkins/jobs/bench_navitia_dev/benchmark.jmx'
-BASE_DIR ='/home/jenkins/jobs/bench_navitia_dev/'
+BASE_DIR = '/var/lib/jenkins/jobs/bench_navitia_dev'
+JMX_SCRIPT= os.path.join(BASE_DIR, 'benchmark.jmx')
 NB_REQUESTS_PER_REGION=10
