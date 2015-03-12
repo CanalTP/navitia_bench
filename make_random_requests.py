b79ee19a2c0e39cfcc53d26aacb9f173a295b1c0
Vincent Lara <lara.vincent@gmail.com>
2015-03-12 15:03:38 +0100

    pouet

--- make_random_requests.py
+++ make_random_requests.py
@@ -13,13 +13,16 @@ s = Server(conf.URL, conf.TOKEN)
 for region in s.regions.itervalues():
     print "Begin to benchmark {}".format(str(region))
     fname = "source_{}.csv".format(region)
-    with open(fname, 'wb') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for i in xrange(conf.NB_REQUESTS_PER_REGION):
-            writer.writerow(region.journeys.make_random_request())
+    path = os.getcwd()
+    source_fname = os.path.join(path, fname)
+    print source_fname
+    with open(source_fname, 'wb') as csvfile:
+        with csv.writer(csvfile, delimiter=',') as writer:
+            for i in xrange(conf.NB_REQUESTS_PER_REGION):
+                writer.writerow(region.journeys.make_random_request())
     result_file = "journeys_{}.jtl".format(str(region))
     l = ["-n", "-t", conf.JMX_SCRIPT,
-            "-Jjourneys_results", result_file, "-Jjourneys_dataset", fname,
+            "-Jjourneys_results", result_file, "-Jjourneys_dataset", source_fname,
             "-Jserver_url", conf.URL, "-Jserver_port", conf.PORT,
             "-Jserver_key", conf.TOKEN, "-Jregion_name", str(region)]
     os.system("sh {} {}".format(conf.JMETER_EXEC, " ".join(l)))
