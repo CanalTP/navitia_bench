9f521a3f4b60bc3f259d6a46851e63fc59e689a7
Vincent Lara <lara.vincent@gmail.com>
2015-03-12 11:00:24 +0100

    fix settings

--- dev_settings.py
+++ dev_settings.py
@@ -1,6 +1,6 @@
 TOKEN='da821360-5fdc-47cb-acc3-9025e56fd711'
-URL='http://navitia2-ws.ctp.dev.canaltp.fr'
-PORT='5000'
+URL='navitia2-ws.ctp.dev.canaltp.fr'
+PORT='80'
 JMX_SCRIPT='/home/jenkins/jobs/bench_navitia_dev/benchmark.jmx'
 BASE_DIR ='/home/jenkins/jobs/bench_navitia_dev/'
 NB_REQUESTS_PER_REGION=10
